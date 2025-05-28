b5585f208a84009715647722b079554a4982915f
iamayac <ayachristia@gmail.com>
2025-05-28 15:06:07 +0200

    meb

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../src/styles/style.scss'
+import './styles/style.scss'
 import { RouterProvider } from 'react-router'
 import router from './router.jsx'
 
