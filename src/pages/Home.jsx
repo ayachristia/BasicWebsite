b0e1df3f3317492ce1177b23dfb1dff1826889b1
iamayac <ayachristia@gmail.com>
2025-05-28 14:15:49 +0200

    meb

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import Hero from "../components/Home/Hero"
-import "../styles/style.scss"
 
 
 export default function Home(){
--- src/router.jsx
+++ src/router.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Contact from "./pages/Contact";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles/style.scss"
+
 
 
 
